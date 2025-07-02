27670747f371f36d2a0db434551b12eb27831d86
vgsml <gribanov.vladimir@gmail.com>
2025-07-02 14:32:47 +0200

    fix: robots

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,6 +69,13 @@ const config: Config = {
           priority: 0.5,
           ignorePatterns: ['/tags/**'],
           filename: 'sitemap.xml',
+          createSitemapItems: async (params) => {
+            const {defaultCreateSitemapItems, ...rest} = params;
+            const items = await defaultCreateSitemapItems(rest);
+            return items.filter((item) => 
+              !item.url.includes('/docs/docs/') 
+            );
+          }
         },
       } satisfies Preset.Options,
     ],
